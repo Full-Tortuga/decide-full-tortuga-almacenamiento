7f2cb058fd61a07ae5a5d4941bb0b35304b83ea6
Manuel Muñoz España <72939269+manmunesp12@users.noreply.github.com>
2021-12-06 17:50:55 +0100

    Revert "Feature/12 aviso modificacion voto"

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import BoothView
-from .views import PruebaView
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
-    path('prueba/', PruebaView.as_view(), name='prueba'),
 ]
