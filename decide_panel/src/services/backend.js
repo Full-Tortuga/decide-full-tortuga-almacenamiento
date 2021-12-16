fd48f7e6ceaa7557ee8c948ff7a6141b1cfb39db
Javitoox <javmarfer3@alum.us.es>
2021-12-16 15:16:12 +0100

    feat: Añade búsqueda de votaciones y redireccionamiento
    Se ha implementado un redireccionamiento en decide, en el cual si accedes a la ruta /panel de decide se abrirá en panel de control implementado en React.
    
    Además, en el panel de control, se está trabajando en el listado de las votaciones existentes y el filtrado de las mismas con múltiples opciones en tiempo real. De momento se ha conseguido la interacción con la API de decide para darnos los detalles de las votaciones con un id concreto. En panel se ha implementado un formulario para buscar dichas votaciones. De momento este formulario no tiene funcionalidad.

--- decide_panel/src/services/backend.js
+++ decide_panel/src/services/backend.js
@@ -6,6 +6,10 @@ const Api = {
   connection_test() {
     return axios.get(URI_BACKEND + "admin/").then((res) => res.status);
   },
+
+  get_voting(id) {
+    return axios.get(URI_BACKEND + "voting/?id=" + id).then((res) => res.data);
+  },
 };
 
 export default Api;
