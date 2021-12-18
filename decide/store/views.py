5708f8df2eb7de65fd76fdbfc47da6a3dbbe7903
manmunesp12 <manmunesp1@alum.us.es>
2021-12-18 20:43:56 +0100

    feat: Modificación del proceso de edición de voto
    Se ha modificado el proceso de edición de un voto haciendo que el voto ya registrado se borre antes de guardar el nuevo evitando así la machacación del voto.
    
    Co-authored-by: Daniel Catroviejo Naranjo <dancasnar1@alum.us.es>

--- decide/store/views.py
+++ decide/store/views.py
@@ -1,14 +1,22 @@
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from tkinter import messagebox
+import tkinter as tk
 import django_filters.rest_framework
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.request import Request
 
 from .models import Vote
 from .serializers import VoteSerializer
 from base import mods
 from base.perms import UserIsStaff
+
+
 
 
 class StoreView(generics.ListAPIView):
@@ -33,6 +41,7 @@ class StoreView(generics.ListAPIView):
         voting = mods.get('voting', params={'id': vid})
         if not voting or not isinstance(voting, list):
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+        
         start_date = voting[0].get('start_date', None)
         end_date = voting[0].get('end_date', None)
         not_started = not start_date or timezone.now() < parse_datetime(start_date)
@@ -58,15 +67,45 @@ class StoreView(generics.ListAPIView):
         if perms.status_code == 401:
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        a = vote.get("a")
-        b = vote.get("b")
+        #Comprobamos que el voto está registrado
+        voto_registrado = Vote.objects.filter(voting_id=vid, voter_id=uid)
+        
+        if voto_registrado:
+            root = tk.Tk()
+            root.withdraw()
+            respuesta1=messagebox.askokcancel(title="¡Cuidado!", message="Se ha encontrado un voto tuyo en esta votación, ¿desea almacenar este voto como nueva respuesta?")
+            #En caso de aceptar la modificación se realiza la modificación del voto
+            if respuesta1==True:
 
-        defs = { "a": a, "b": b }
-        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
-                                          defaults=defs)
-        v.a = a
-        v.b = b
+                #Se elimina  el voto anterior registrado
+                for vt in voto_registrado:
+                    vt.delete()
 
-        v.save()
+                a = vote.get("a")
+                b = vote.get("b")
 
-        return  Response({})
+                defs = { "a": a, "b": b }
+                v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+                                                defaults=defs)
+                v.a = a
+                v.b = b
+
+                v.save()
+                #Se elimina la ventana para poder volver a aparecer en caso de volver a querer editar el voto
+                root.destroy()
+                return  Response({})
+            else:
+                root.destroy()
+                return  Response({}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            a = vote.get("a")
+            b = vote.get("b")
+
+            defs = { "a": a, "b": b }
+            v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+                                            defaults=defs)
+            v.a = a
+            v.b = b
+
+            v.save()
+            return  Response({})
