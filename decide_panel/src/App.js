b19449aa0e8819c8ade722a6f93d5c1b3c61d0ef
NoSoyDani <dsgpolaris55@gmail.com>
2021-12-27 18:38:41 +0100

    feat: Añade funcionalidad base
    Se han creado los archivos necesarios para realizar la funcionalidad
    así como, se ha creado la llamada a la API necesaria para mostrar datos
    sobre las votaciones.

--- decide_panel/src/App.js
+++ decide_panel/src/App.js
@@ -2,17 +2,16 @@ import Views from "./components/Views";
 import "./css/App.css";
 import { TabPanel } from "primereact/tabview";
 import ConnectionTest from "./components/ConnectionTest";
-
+import Voting from "./components/Voting";
+import Votes from "./components/Votes";
 function App() {
   return (
     <Views>
-      <TabPanel header="Listado 1" rightIcon="pi pi-table">
-        {/*Componente personalizado de listado*/}
-        <p>Listado 1</p>
+      <TabPanel header="Votaciones" rightIcon="pi pi-inbox">
+        <Voting />
       </TabPanel>
-      <TabPanel header="Listado 2" rightIcon="pi pi-table">
-        {/*Componente personalizado de listado*/}
-        <p>Listado 2</p>
+      <TabPanel header="Votos" rightIcon="pi pi-inbox">
+        <Votes />
       </TabPanel>
       <TabPanel header="Estadísticas" rightIcon="pi pi-chart-bar">
         {/*Componente personalizado de gráficos*/}
--- decide_panel/src/services/backend.js
+++ decide_panel/src/services/backend.js
@@ -6,6 +6,15 @@ const Api = {
   connection_test() {
     return axios.get(URI_BACKEND + "admin/").then((res) => res.status);
   },
+
+  get_census(voting_id) {
+    return axios
+      .get(URI_BACKEND + "census/" + voting_id + "/")
+      .then((res) => res.data);
+  },
+  get_votes(voting_id){
+    return axios.get(URI_BACKEND + "voting/?id=" + voting_id).then((res) => res.data);
+  }
 };
 
 export default Api;
