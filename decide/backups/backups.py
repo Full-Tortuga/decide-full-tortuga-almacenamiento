88df44ee04de5108edd1302b0f3fd065dd8e15f5
javivm17 <javiervilarinyomayo.cr@gmail.com>
2021-12-27 10:54:46 +0100

    test: Añade test unitario para la creación de backups
    Se ha añadido un test unitario que prueba que el script de creación de copias de seguridad funciona correctamente.
    
    Para lanzar unicamente este test usar: ./manage.py test backups

--- decide/backups/backups.py
+++ decide/backups/backups.py
@@ -1,17 +1,15 @@
 import os
 import time
 
-dir = './decide/backups/backups/'
-
-def location():
+def location(dir):
   return dir + time.strftime("%d-%m-%Y-%H:%M:%S")
 
-def run_backup():
-    run_backup = "mongodump --out " + location()
+def run_backup(dir):
+    print("generating mongo backup...")
+    run_backup = "mongodump --out " + location(dir)
     os.system(run_backup)
 
-print("generating mongo backup...")
 
-run_backup()
+
 
  
