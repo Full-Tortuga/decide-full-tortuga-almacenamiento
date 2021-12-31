1baeadcea529f55d29164cc9a75b9a93691d024d
Evelyn Gisele Yugsi Yugsi <evelynyugsiyugsi@gmail.com>
2021-12-31 14:23:30 +0100

    feat: Añade backend necesario para la restauración de una copia de seguridad
    Se han añadido dos nuevos endpoints en la API de decide, necesarios para consultar una lista de las copias de seguridad
    existentes y para poder restaurar alguna de dichas copias de seguridad.

--- decide/backups/urls.py
+++ decide/backups/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     #It isn already implemented in the views.py
     path('', views.index),
     path('create', backups.CreateBackup.as_view(), name='create_backup'),
+    path('list', backups.RestoreBackup.as_view(), name='list_backups'),
+    path('restore/<str:backup_name>', backups.RestoreBackup.as_view(), name='restore_backup')
 ]
