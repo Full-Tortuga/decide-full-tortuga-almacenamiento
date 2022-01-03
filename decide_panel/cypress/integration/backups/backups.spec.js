b593c4b2d7bd04fde38ee89b2e6e0f5d0f329d89
javivm17 <javiervilarinyomayo.cr@gmail.com>
2022-01-03 13:45:05 +0100

    test: Añade nuevos test de backups
    Se han añadidos nuevos test end-to-end para comprobar la correcta funcionalidad de los backups.

--- decide_panel/cypress/integration/backups/backups.spec.js
+++ decide_panel/cypress/integration/backups/backups.spec.js
@@ -1,3 +1,4 @@
+Cypress.config('defaultCommandTimeout', 20000);
 describe("Conection test",()=>{
     beforeEach(()=>{
         cy.visit("http://localhost:3000/");
@@ -6,6 +7,22 @@ describe("Conection test",()=>{
     it('backups page can be opened', ()=>{
         cy.contains('Backups').click()
         cy.contains('Generar nuevo backup')
+    })
+
+    it("backups can be generated", ()=>{
+        cy.contains('Backups').click()
+        cy.contains('Generar nuevo backup').click()
+        cy.contains('Se ha creado correctamente el backup')
+    })
+    it("dropdown to restore backup exists and is clickable", ()=>{
+        cy.contains('Backups').click()
+        cy.get('.p-dropdown-trigger-icon').click()
+    })
+    it("dropdown to restore backup has a restore option", ()=>{
+        cy.contains('Backups').click()
+        cy.get('.p-dropdown-trigger-icon').click()
+        cy.get('.p-dropdown-item').first().click()
+        cy.contains('Se ha restaurado correctamente el backup')
     })
 }
 )
