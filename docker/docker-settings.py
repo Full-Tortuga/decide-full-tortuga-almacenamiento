c3a81fd4dbf8b192b61b657a355b805a655a9c62
NoSoyDani <dsgpolaris55@gmail.com>
2022-01-07 21:10:41 +0100

    feat: Crea imagen de docker a partir de Dockerfile
    Se ha modificado el Dockerfile para crear una imagen a semejanza del
    proyecto local.
    
    Co-authored-by: Daniel Castroviejo Naranjo <dancasnar1@alum.us.es>
    Co-authored-by: Manuel Muñoz España <manmunesp1@alum.us.es>

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -1,45 +1,149 @@
+import os
+
 DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'decide',
-        'PORT': '27017',
-        'CLIENT': {
-           'host': '10.5.0.1',
-        }
-
-    }
-}
-
 
 STATIC_ROOT = '/app/static/'
 MEDIA_ROOT = '/app/static/media/'
 ALLOWED_HOSTS = ['*']
 
+BASEURL = 'http://localhost'
+
+APIS = {
+    'authentication': 'http://localhost',
+    'base': 'http://localhost',
+    'booth': 'http://localhost',
+    'census': 'http://localhost',
+    'mixnet': 'http://localhost',
+    'postproc': 'http://localhost',
+    'store': 'http://localhost0',
+    'visualizer': 'http://localhost',
+    'voting': 'http://localhost',
+    'backups': 'http://localhost',
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'CLIENT': {
+           'host': os.environ.get('DATABASE_HOST'),
+           'username':  os.environ.get('DATABASE_USER'),
+           'password': os.environ.get('DATABASE_PASSWORD'),
+	       'SSL': 'true'
+        }
+    }
+}
+
+
 # Modules in use, commented modules that you won't use
 MODULES = [
-    'authentication',
+   'authentication',
     'base',
     'booth',
     'census',
+    'voting',
     'mixnet',
     'postproc',
     'store',
     'visualizer',
-    'voting',
+    'backups',
 ]
 
-BASEURL = 'http://10.5.0.1:8000'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
 
-APIS = {
-    'authentication': 'http://10.5.0.1:8000',
-    'base': 'http://10.5.0.1:8000',
-    'booth': 'http://10.5.0.1:8000',
-    'census': 'http://10.5.0.1:8000',
-    'mixnet': 'http://10.5.0.1:8000',
-    'postproc': 'http://10.5.0.1:8000',
-    'store': 'http://10.5.0.1:8000',
-    'visualizer': 'http://10.5.0.1:8000',
-    'voting': 'http://10.5.0.1:8000',
-}
+ROOT_URLCONF = 'decide.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'decide.wsgi.application'
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
+
+# Versioning
+ALLOWED_VERSIONS = ['v1', 'v2']
+DEFAULT_VERSION = 'v1'
+
+try:
+    from local_settings import *
+except ImportError:
+    print("local_settings.py not found")
+
+# loading jsonnet config
+if os.path.exists("config.jsonnet"):
+    import json
+    from _jsonnet import evaluate_file
+    config = json.loads(evaluate_file("config.jsonnet"))
+    for k, v in config.items():
+        vars()[k] = v
+
+
+INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+
+
